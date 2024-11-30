47dc1cb467236d27391cc8c5848733bb1b77017f
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2024-11-30 09:31:35 +0530

    added MCA

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,13 +9,12 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi ✋🏻, I am <span className="purple">Saksham Joshi </span>
             from  Uttarakhand, India. I am {(new Date().getFullYear()-2003)} years old.
-            {/* <br />
+            <br />
             <br/>
-            Currently pursuing  Master's in Computer Application from G.B. Pant University of Agriculture and Technology, Pantnagar.
-            <br/> */}
+            Currently pursuing  Master's in Computer Applications from G.B. Pant University of Agriculture and Technology, Pantnagar, Uttarakhand.
             <br/>
             <br/>
-            I hold a Bachelor’s degree👨🏻‍🎓 in Computer Application from Graphic Era Hill University, Bhimtal.
+            I hold a Bachelor’s degree👨🏻‍🎓 in Computer Applications from Graphic Era Hill University, Bhimtal.
             <br />
             <br />
             As a passionate self-taught <span className="purple">Compiler Engineer</span>💻, I believe in diving deep into every concept to unlock true mastery.
