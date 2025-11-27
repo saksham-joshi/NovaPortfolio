c0f8ad61569bb12477bbebb9e9401f30da87df88
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-11-27 22:29:28 +0530

    Changed: footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,11 +5,23 @@ import { greeting } from "../../portfolio.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
+  const theme = props.theme;
+  const footer_text = `< Star this Portfolio on GitHub />`;
   return (
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Beautified with <span role="img">❤️</span> by {greeting.title}
+          <a 
+            href={greeting.portfolio_repository}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="git-repo-url"
+            style={{
+              color: theme.text,
+            }}
+          >
+           {footer_text}
+          </a>
         </p>
       </Fade>
     </div>
