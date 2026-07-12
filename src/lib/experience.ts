6ed81a5311e7320c05cd50bd296c7c996c1bfd4e
SAKSHAM JOSHI <109752898+saksham-joshi@users.noreply.github.com>
2026-07-12 07:40:30 +0530

    Updated: experience sectoin

--- src/lib/experience.ts
+++ src/lib/experience.ts
@@ -2,10 +2,10 @@ import ImageArterns from '../assets/images/experience/arterns-logo.png'
 import ImageCodeKaroYaaro from '../assets/images/experience/codekaroyaaro-logo.png'
 import ImageCosmoPortfolio from '../assets/images/experience/cosmo-portfolio.png'
 import ImageMasterPortfolio from '../assets/images/experience/master-portfolio.png'
-import ImageHaveYourPR from '../assets/images/experience/haveyourpr.png'
-import ImageUniteWings from '../assets/images/experience/unite-wings.png'
-import ImageMaryornEsports from '../assets/images/experience/maryorn-esports.png'
-import ImageMaryornLogix from '../assets/images/experience/maryorn-logix.png'
+// import ImageHaveYourPR from '../assets/images/experience/haveyourpr.png'
+// import ImageUniteWings from '../assets/images/experience/unite-wings.png'
+// import ImageMaryornEsports from '../assets/images/experience/maryorn-esports.png'
+// import ImageMaryornLogix from '../assets/images/experience/maryorn-logix.png'
 
 export const DATA_EXPERIENCE = {
   title: 'Experience',
@@ -13,55 +13,55 @@ export const DATA_EXPERIENCE = {
   description:
     'Worked across multiple startups, completed internships, and contributed to open-source projects, consistently delivering high-quality softwares and great results.',
   sections: [
-    {
-      title: 'Startups',
-      experiences: [
-        {
-          title: 'Founder & CTO',
-          company: 'HaveYourPR Agency',
-          orgUrl: 'https://haveyourpr.com',
-          logo: ImageHaveYourPR,
-          duration: 'June 2026 - Present',
-          location: 'On-site',
-          description:
-            'Founded this PR agency with three other co-founders. Led the entire technical development of the startup, including frontend, backend, database design, authentication, deployment, domain registration and configuration, custom domain email setup, and the development of a comprehensive admin panel to manage clients and automate internal operations. Additionally, developed multiple PR strategies to help clients expand their reach and acquire more customers. Built a strong professional network with influencers, local businesses, small-scale enterprises, and politicians.',
-          color: '#ffffff'
-        },
-        {
-          title: 'Founder',
-          company: 'Unite Wings Educational Confederation',
-          orgUrl: 'https://unitewings.com',
-          logo: ImageUniteWings,
-          duration: 'July 2024 - Sept 2024',
-          location: 'Hybrid',
-          description:
-            '[FAILED STARTUP] In this startup, we provided resume-building services, mock interviews, and educational courses to students. We hired more than 20 interns, including content writers, C++ engineers, software testers, front-end developers, and HR interns. Within a month, we gained more than 300 followers on LinkedIn. We also launched the SRON Contributors Program, through which we recruited C++ engineers and software testers to test and improve my programming language, SRON. I left the startup due to the incompetence and mismanagement of my co-founder, and the startup eventually failed.',
-          color: '#ffffff'
-        },
-        {
-          title: 'Founder',
-          company: 'Maryorn Esports',
-          orgUrl: '',
-          logo: ImageMaryornEsports,
-          duration: 'March 2022 - June 2022',
-          location: 'Online',
-          description:
-            "[FAILED STARTUP] Founded an esports organization focused on PUBG Mobile (now BGMI in India). Created and managed the organization\'s social media accounts and a professionally structured Discord server. Recruited and managed two Tier-2 esports lineups, each consisting of five players, and regularly registered them for daily scrims and tournaments to support their competitive practice. Organized two esports events. The first was a 1v1 Team Deathmatch (TDM) Tournament, which attracted 20 participants. The winner received a certificate and free entry to the second tournament. The second event was unsuccessful due to a ₹10 registration fee, resulting in no participant registrations. Owing to this outcome and increasing academic commitments, I decided to discontinue the organization.",
-          color: '#ffffff'
-        },
-        {
-          title: 'Founder',
-          company: 'Maryorn Logix',
-          orgUrl: '',
-          logo: ImageMaryornLogix,
-          duration: 'June 2020 - Nov 2020',
-          location: 'Online',
-          description:
-            "[FAILED STARTUP] Launched a solo software publishing start-up focusing on Android applications. Created custom Android applications on the AppGeyser.com and published on the Google Play Store. Successfully released five applications: M4 (web browser), Wall-X (wallpaper app), Crossy (crossword puzzle game), Task (to-do app) and Truth and Dare (bottle game). Google announced Android 11 in September 2020 and required developers to upgrade their apps to satisfy the latest platform requirements. At that point my programming experience was zero, and I was unable to make the necessary updates myself. I wrote to AppGeyser's support team, but unfortunately, I didn't get any response. So my google play console account got suspended at the end because the apps were not meeting the updated requirements and and my startup failed.",
-          color: '#ffffff'
-        }
-      ]
-    },
+    // {
+    //   title: 'Startups',
+    //   experiences: [
+    //     {
+    //       title: 'Founder & CTO',
+    //       company: 'HaveYourPR Agency',
+    //       orgUrl: 'https://haveyourpr.com',
+    //       logo: ImageHaveYourPR,
+    //       duration: 'June 2026 - Present',
+    //       location: 'On-site',
+    //       description:
+    //         'Founded this PR agency with three other co-founders. Led the entire technical development of the startup, including frontend, backend, database design, authentication, deployment, domain registration and configuration, custom domain email setup, and the development of a comprehensive admin panel to manage clients and automate internal operations. Additionally, developed multiple PR strategies to help clients expand their reach and acquire more customers. Built a strong professional network with influencers, local businesses, small-scale enterprises, and politicians.',
+    //       color: '#ffffff'
+    //     },
+    //     {
+    //       title: 'Founder',
+    //       company: 'Unite Wings Educational Confederation',
+    //       orgUrl: 'https://unitewings.com',
+    //       logo: ImageUniteWings,
+    //       duration: 'July 2024 - Sept 2024',
+    //       location: 'Hybrid',
+    //       description:
+    //         '[FAILED STARTUP] In this startup, we provided resume-building services, mock interviews, and educational courses to students. We hired more than 20 interns, including content writers, C++ engineers, software testers, front-end developers, and HR interns. Within a month, we gained more than 300 followers on LinkedIn. We also launched the SRON Contributors Program, through which we recruited C++ engineers and software testers to test and improve my programming language, SRON. I left the startup due to the incompetence and mismanagement of my co-founder, and the startup eventually failed.',
+    //       color: '#ffffff'
+    //     },
+    //     {
+    //       title: 'Founder',
+    //       company: 'Maryorn Esports',
+    //       orgUrl: '',
+    //       logo: ImageMaryornEsports,
+    //       duration: 'March 2022 - June 2022',
+    //       location: 'Online',
+    //       description:
+    //         "[FAILED STARTUP] Founded an esports organization focused on PUBG Mobile (now BGMI in India). Created and managed the organization\'s social media accounts and a professionally structured Discord server. Recruited and managed two Tier-2 esports lineups, each consisting of five players, and regularly registered them for daily scrims and tournaments to support their competitive practice. Organized two esports events. The first was a 1v1 Team Deathmatch (TDM) Tournament, which attracted 20 participants. The winner received a certificate and free entry to the second tournament. The second event was unsuccessful due to a ₹10 registration fee, resulting in no participant registrations. Owing to this outcome and increasing academic commitments, I decided to discontinue the organization.",
+    //       color: '#ffffff'
+    //     },
+    //     {
+    //       title: 'Founder',
+    //       company: 'Maryorn Logix',
+    //       orgUrl: '',
+    //       logo: ImageMaryornLogix,
+    //       duration: 'June 2020 - Nov 2020',
+    //       location: 'Online',
+    //       description:
+    //         "[FAILED STARTUP] Launched a solo software publishing start-up focusing on Android applications. Created custom Android applications on the AppGeyser.com and published on the Google Play Store. Successfully released five applications: M4 (web browser), Wall-X (wallpaper app), Crossy (crossword puzzle game), Task (to-do app) and Truth and Dare (bottle game). Google announced Android 11 in September 2020 and required developers to upgrade their apps to satisfy the latest platform requirements. At that point my programming experience was zero, and I was unable to make the necessary updates myself. I wrote to AppGeyser's support team, but unfortunately, I didn't get any response. So my google play console account got suspended at the end because the apps were not meeting the updated requirements and and my startup failed.",
+    //       color: '#ffffff'
+    //     }
+    //   ]
+    // },
     {
       title: 'Internships',
       experiences: [
@@ -73,7 +73,7 @@ export const DATA_EXPERIENCE = {
           duration: 'May 2025 - Nov 2025',
           location: 'Hybrid',
           description:
-            'Created and improved high-performance web apps with Elysia.js and Node.js, with a focus on type safety from start to finish and fast execution. Using Express.js and Flask, I designed and maintained strong RESTful APIs that made sure frontend and backend services could talk to each other without any problems. Added Firebase for cloud functions, real-time database management, and authentication, which made it easier to keep data in sync across platforms. Worked together on the entire software development life cycle (SDLC), from making prototypes to putting scalable server-side solutions into use.',
+            'Developed an employee verification API using Bun, Elysia.js, Redis, and PostgreSQL, capable of handling more than 20,000 requests per second. Developed a static file hosting service to quickly download images, audios, videos, documents, etc., using FastAPI, Python, and Ngrok, capable of handling 10,000 requests per second. Designed safe, secure, and precise schemas for PostgreSQL databases to restrict access to certain roles using Row-Level Security. Implemented the entire login system using Next.js and Redis, writing Lua scripts for Redis (deployed on Upstash) to power an OTP verification login flow for a client\'s system while ensuring rate limiting to prevent attacks. Maintained client websites and ensured 100% uptime of their services throughout my internship. Set up the email system and created multiple custom emails for a client\'s startup using Lark Suite by updating the MX, A, and TXT records in the client\'s Hostinger DNS/nameservers.',
           color: '#000000'
         },
         {
